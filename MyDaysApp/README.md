f8df3dfb61853e09fe27ba167132ad1386d1a622
KarVarr <megatr9n@gmail.com>
2022-12-07 22:11:36 +0300

    added readme and img, Date and result in the label

---
+++ MyDaysApp/README.md
@@ -0,0 +1,5 @@
+#  <#Title#>
+## The first app from The Sfiwtbook.org
+### Learning storyboard, assistant, constrain
+
+<img src="MyDaysApp/ScreeniPhone14.png" alt="img" width="500"/>
--- MyDaysApp/ViewController.swift
+++ MyDaysApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var infoLabel: UILabel!
+    @IBOutlet var resultButton: UIButton!
+    
+    var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultButton.layer.cornerRadius = 12
     }
-
-
+    
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+    }
+    
+    @IBAction func resultButtonTapped(_ sender: UIButton) {
+        infoLabel.text = "You lived on this Earth \(numberOfDays) days"
+    }
+    
 }
 
